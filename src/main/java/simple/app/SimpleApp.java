9d75a36860e4cb8948071a7a8148118ec203f8e1
Adam Bialobrzewski <Adam_Bialobrzewski@epam.com>
2020-03-05 10:48:38 +0100

    Initial project version

---
+++ src/main/java/simple/app/SimpleApp.java
@@ -0,0 +1,11 @@
+package simple.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApp {
+    public static void main(final String[] args) {
+        SpringApplication.run(SimpleApp.class, args);
+    }
+}
